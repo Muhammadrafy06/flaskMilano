6ce6d9b0c16bd4b05025a7209ded22bf4a247e74
Muhammadrafy06 <muhammad.abdul@iisgalvanimi.edu.it>
2023-03-07 11:35:17 +0000

    image name changed

--- Main.py
+++ Main.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html")
 
+@app.route('/descrizione', methods=['GET'])
+def bye_world():
+    return render_template("testo.html")
+
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8080, debug=True)
